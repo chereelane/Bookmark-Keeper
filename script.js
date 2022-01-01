a6d2f997a249b22d46e58f8051f804a7d25a5031
Cheree <38049977+chereelane@users.noreply.github.com>
2021-12-31 20:26:46 -0500

    added functioonality for the modal to disappear when clicked anywhere in the window

--- script.js
+++ script.js
@@ -15,3 +15,4 @@ function showModal () {
 // Modal Event Listeners
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => console.log(e.target));
